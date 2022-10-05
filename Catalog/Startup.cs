0cb059b8755b448400a60ba25d39a0b5aeec62fd
donkennie <ajeigbekehinde160@gmail.com>
2022-10-05 07:14:59 +0530

    MongoDb configuration in startup

--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -1,4 +1,5 @@
 using Catalog.Repositories;
+using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,12 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IMongoClient>(ServiceProvider =>
+            {
+
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
 
             services.AddSingleton<IItemsRepository, InMemItemsRepository>();
 
